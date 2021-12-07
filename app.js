ff4af6c62fe2457afb1d47f7f7d1df7b1c43b839
Jack Parrish <jparrish331@gmail.com>
2021-12-07 15:34:13 -0600

    navbar working now

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 const navBarLinks = document.getElementsByClassName("mobile-nav-links")[0];
-const navChildren = navBarLinks.children;
 const navCheckbox = document.getElementById("nav-toggle");
 
-const navChildrenArray = [...navChildren];
-
-navChildrenArray.forEach(el => el.addEventListener("blur", removeNavChecked(navBarLinks,navCheckbox)));
-
-function removeNavChecked(container,target) {
-    const focusedWithin = container.matches(':focus-within');
+navBarLinks.addEventListener("focusout", function() {
+    const container = document.getElementsByClassName("mobile-nav-links")[0];
+    const focusedWithin = container.matches(":focus-within");
     if (!focusedWithin) {
-        target.checked = false;
-        console.log("we got here");
+        document.getElementById("nav-toggle").checked = false;
     }
-}
+});
